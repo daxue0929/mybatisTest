aca3025624f27d94079ca44c7780ba19fbeddcf6
Administrator <ainidehuiji@163.com>
2019-06-15 20:29:52 +0800

    测试多参数查询

--- src/main/java/mapper/BlogArticleMapper.java
+++ src/main/java/mapper/BlogArticleMapper.java
@@ -1,8 +1,12 @@
 package mapper;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public interface BlogArticleMapper {
     //通过外键user_id查询,当前user_id用户发表的博文列表有哪些
     public ArrayList queryBlogArticleByUserId(int userId);
+
+    //查询两个用户发表的全部博文
+    public ArrayList queryBlogArticleByUserIds(HashMap hashMap);
 }
--- src/main/java/test1/Test3.java
+++ src/main/java/test1/Test3.java
@@ -3,6 +3,7 @@ package test1;
 import mapper.BlogArticleMapper;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
 import util.SqlSessionFactoryUtil;
 
 import java.util.ArrayList;
@@ -10,6 +11,44 @@ import java.util.HashMap;
 
 public class Test3 {
 
+    @Test
+    public void test5() {
+        SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtil.getInstance();
+        SqlSession session = sqlSessionFactory.openSession();
+        BlogArticleMapper blogArticleMapper = session.getMapper(BlogArticleMapper.class);
+
+
+
+    }
+
+
+    /**
+     * 测试方法,多参数传输测试方法,以HashMap的方式传输多参数
+     */
+    @Test
+    public void test4(){
+        SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtil.getInstance();
+        SqlSession session = sqlSessionFactory.openSession();
+        BlogArticleMapper blogArticleMapper = session.getMapper(BlogArticleMapper.class);
+
+        HashMap hashMap = new HashMap();
+
+        hashMap.put("userId1", 1001);
+        hashMap.put("userId2",1007);
+
+        ArrayList blogArticleArrayList = blogArticleMapper.queryBlogArticleByUserIds(hashMap);
+        if (!blogArticleArrayList.isEmpty()){
+            System.out.println("该用户发表了" + blogArticleArrayList.size() + "篇博文!");
+            for(Object blogArticleItem : blogArticleArrayList){
+                HashMap blogArticleItemHashMap = (HashMap)blogArticleItem;
+                System.out.println(blogArticleItemHashMap.get("article_title"));
+            }
+        }else{
+            System.out.println("当前用户没有发表的博客文章");
+        }
+    }
+
+    //主方法
     public static void main(String[] args) {
         SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtil.getInstance();
         SqlSession session = sqlSessionFactory.openSession();
@@ -19,6 +58,7 @@ public class Test3 {
 
         //测试查询当前用户的博文列表有哪些
         ArrayList<HashMap> blogArticleArr = blogArticleMapper.queryBlogArticleByUserId(1007);
+
         if (!blogArticleArr.isEmpty()){
             System.out.println("该用户发表了" + blogArticleArr.size() + "篇博文!");
             for(Object blogArticleItem : blogArticleArr){
@@ -27,7 +67,5 @@ public class Test3 {
         }else{
             System.out.println("当前用户没有发表的博客文章");
         }
-
-
     }
 }
